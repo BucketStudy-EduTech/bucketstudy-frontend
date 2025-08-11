0c6c77e1e884d7ae4489f46bcd7043218b9572d4
ErSakshiBhor <sakshibhor2005@gmail.com>
2025-08-11 22:07:02 +0530

    changed login ,signup form

--- src/Pages/Challange/challenge/Challenge.jsx
+++ src/Pages/Challange/challenge/Challenge.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
+
+
+import React from 'react';
 import './Challenge.css';
 
 function Challenge() {
   return (
-    <div className=''>
-      <div className='first font-extrabold text-center pt-8'>
-        <h1 className='text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 via-pink-300 to-red-700 border-3'>Challenges</h1>
-        <h5 className='italic text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r  mt-1 from-yellow-400 via-amber-500 to-orange-700"'>Test Your Skills</h5>
-      </div>
+    <div className='first'>
+      <h1 className='main-title'>Challenges</h1>
+      <h5 className='subtitle'>Test Your Skills</h5>
     </div>
-  )
+  );
 }
 
-export default Challenge
+export default Challenge;
